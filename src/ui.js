51adbac03af96ec05ec82d7f2d22d6a4d250f11a
David <drgdeved@gmail.com>
2026-09-05 00:29:31 +0300

    Implement todos window with todo display and sorting

--- src/ui.js
+++ src/ui.js
@@ -38,7 +38,6 @@ let updateSideBar = () => {
 //          show the groups of the section in the main body
 let selectedProjectAndSection = ["myRoutine", "morning"];
 let updateMainBody = () => {
-    console.log(selectedProjectAndSection);
     
     let groups = LevelSelector(selectedProjectAndSection[0], selectedProjectAndSection[1]).group
     let body = document.querySelector(".content-area")
@@ -46,7 +45,7 @@ let updateMainBody = () => {
     for (let group of groups) {
         body.innerHTML += `
             <div class="group">
-                <h1 class="group-title"> ${group.name}</h1>
+                <h1 class="group-title">${group.name}</h1>
                 <ul class="group-todos">
                     ${group.todos.map(item => `<li class="group-todo-item">${item.name}</li>`).join('')}        
                 </ul>
@@ -70,8 +69,55 @@ sideBar.addEventListener('click', (e)=> {
 })
 
 
+//opened window
+let body = document.querySelector("body")
+let window = document.querySelector(".details-window")
+let chosenGroup;
+
+//TODO: finish this function to display group information into an opened window
+let updateWindow= () => {
+    let targetFamilyTree = LevelSelector(selectedProjectAndSection[0], selectedProjectAndSection[1], chosenGroup)
+    let targetTodos = targetFamilyTree.todos
+    let titlePlace = window.querySelector(".window-title")
+    let todosPlace = window.querySelector(".window-todos")
+
+    titlePlace.textContent = targetFamilyTree.group[0].name
+    targetTodos.sort((a, b) => b.priority - a.priority);
+
+    console.log(targetTodos);
+    
+    //render todos:
+    todosPlace.innerHTML = ""
+    for (let todo of targetTodos) {
+        todosPlace.innerHTML += `
+            <div class="window-todo-item" id="${todo.name}">
+                <p class="todo-priority">${todo.priority}</p>
+                <input type="checkbox" ${todo.checked ? 'checked' : ''}> <h2>${todo.name}</h2> 
+                <div class="todo-item-info">
+                    ${todo.description ? `<p class="todo-description">${todo.description}</p>` : ''}
+                    ${todo.dueDate ? `<p class="todo-duedate">${todo.dueDate}</p>` : ''}
+                </div>
+                <button class="delete-todo-btn">DELETE</button>
+            </div>
+        `
+    }
+    
+    
+}
 
 
+//open window || close window
+body.addEventListener('click', (e)=> {
+    if (e.target.classList.contains("group") || e.target.classList.contains("close-window-btn")) {
+        window.classList.toggle("hide")
+    }
+        //TODO: use updateWindow to view info
+        if (e.target.classList.contains("group")) {
+            chosenGroup = e.target.querySelector(".group-title").textContent 
+            updateWindow();
+        }
+        
+})
 
 
 
